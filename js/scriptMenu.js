20a12a9bf185bd3e2cbbf7c3f06805ac6f0cab63
lyzEvelin <lyz.evelin.13@gmail.com>
2019-03-27 05:06:48 -0500

    menu con un solo enlace

---
+++ js/scriptMenu.js
@@ -0,0 +1,6 @@
+var menu="";
+menu += "<a href=\"../index.html\">|HOME|<\/a>";
+menu += "<a href=\"troncales.html\">|TRONCALES|<\/a>";
+menu += "<a href=\"nosotros.html\">|NOSOTROS|<\/a>";
+menu += "<a href=\"contacto.html\">|CONTACTO|<\/a>";
+document.write(menu);
